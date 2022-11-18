9df2bf5845640e851488782e021b42492b8a171f
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-18 11:47:02 +0000

    added 9th kata, not complete yet

---
+++ src/typescript/9-count-occurrence-of-all-letters.ts
@@ -0,0 +1,23 @@
+/*
+function: countLetters(inputText, number)
+parameters: string, number
+output: [string, number][]. Array of sets
+=======================================
+
+DECLARE resultArr to be empty array
+DECLARE uniqueLetters to be = function createUniqueLettersArr(inputString)
+FOR each letter in uniqueLetters
+    letterCount = function countLetteroccurence(inputString)
+    DECLARE letterSet to be equal to array of letter and letterCount values
+    push letterSet to resultArr
+END FOR
+sort resultArr with respect to letterCount values
+
+RETURN resultArr
+
+*/
+function countLetters(inputText: string, inputNumber: number): (string|number)[][] {
+    const resultArr = [["L", 2]]
+
+    return resultArr
+}
